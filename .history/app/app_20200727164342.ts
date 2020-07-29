aa3f5c775323cb11ac364df0279d94e5ee71788e
Nicholas.Arocho <nick@boldlyfine.com>
2020-07-28 17:58:17 -0700

    remove .history files

--- .history/app/app_20200727164342.ts
+++
@@ -1,5 +0,0 @@
-function startGame() {
-  // starting a new game
-  var messagesElement = document.getElementById('messages');
-  
-}
--- .history/app/app_20200727164407.ts
+++
@@ -1,5 +0,0 @@
-function startGame() {
-  // starting a new game
-  var messagesElement = document.getElementById('messages');
-  messagesElement.innerText = 'Welcome to MultiMath! Starting new game...'
-}
